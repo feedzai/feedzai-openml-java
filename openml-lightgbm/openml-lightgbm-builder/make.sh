936be6219c243dace39906663f4a331d39a33c74
Alberto Ferreira <alberto.ferreira@feedzai.com>
2020-07-23 18:01:36 +0100

    Support new custom LIGHTGBM_REPO_URL parameter in make-lightgbm.

--- openml-lightgbm/openml-lightgbm-builder/make.sh
+++ openml-lightgbm/openml-lightgbm-builder/make.sh
@@ -18,29 +18,11 @@
 
 set -e
 
-LIGHTGBM_VERSION="$1"
-LIGHTGBMLIB_VERSION="$2"
+export LIGHTGBM_REPO_URL="$1" # This env var will be consumed by make.sh.
+LIGHTGBM_VERSION="$2"
+LIGHTGBMLIB_VERSION="$3"
 
-# Compare version, when different remove the build folder
-FILE=lightgbmlib_build/__version__
-if [ -f "$FILE" ]; then
-  OLD_VERSION=$(head -n 1 "$FILE")
-  if [ "$LIGHTGBM_VERSION" != "$OLD_VERSION" ]; then
-    echo "Renaming the folder."
-    rm -rf lightgbmlib_build
-  fi
-fi
+echo "Building LightGBM $LIGHTGBM_VERSION as lightgbmlib $LIGHTGBMLIB_VERSION"
+cd make-lightgbm
+bash make.sh "$LIGHTGBM_VERSION" "$LIGHTGBMLIB_VERSION"
 
-# Make LightGBM if it doesn't exist
-DIR=lightgbmlib_build
-if [ ! -d "$DIR" ]; then
-  echo "Entering the folder."
-  cd make-lightgbm
-  echo "Building LightGBM $LIGHTGBM_VERSION as lightgbmlib $LIGHTGBMLIB_VERSION"
-  bash make.sh "$LIGHTGBM_VERSION" "$LIGHTGBMLIB_VERSION"
-  echo "Exiting the folder."
-  cd ..
-  echo "Renaming the folder."
-  mv make-lightgbm/build lightgbmlib_build
-  echo "Finished!"
-fi
