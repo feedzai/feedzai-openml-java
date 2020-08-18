a139d299aa7812338c401c60c3e82042ad4e0f06
Sheng Wang <sheng.wang@feedzai.com>
2020-08-18 11:16:10 +0100

    Changed maven plugin architecture

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,5 @@
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ ! -z "$TRAVIS_TAG" ]; then
     mvn deploy -Prelease --settings .m2/settings.xml -DskipTests=true
-    echo "uploading make-lightgbm jar..."
-    mvn deploy:deploy-file -Prelease --settings .m2/settings.xml -DskipTests=true -DgeneratePom=false -DpomFile=openml-lightgbm-meta-module/lightgbm-builder/make-lightgbm/build/pom.xml -Dfile=openml-lightgbm-meta-module/lightgbm-builder/make-lightgbm/build/lightgbmlib.jar
     exit $?
 fi
