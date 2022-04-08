1872caa7b31ea1afb2363a5365017d757929086c
Paulo Pereira <paulo_pereira_6@hotmail.com>
2022-04-08 15:36:39 +0100

    Fix validation when training H2o models (#103) (#106)
    * Fix validation when training H2o models
    
    Co-authored-by: Paulo Pereira <paulo.pereira@feedzai.com>

--- openml-h2o/src/main/java/com/feedzai/openml/h2o/algos/H2OIsolationForestUtils.java
+++ openml-h2o/src/main/java/com/feedzai/openml/h2o/algos/H2OIsolationForestUtils.java
@@ -40,6 +40,12 @@ public class H2OIsolationForestUtils extends AbstractUnsupervisedH2OParamUtils<I
             ParametersBuilderUtil.getParametersFor(IsolationForestParametersV3.class, water.bindings.pojos.IsolationForestParametersV3.class);
 
     /**
+     * The complete collection of model parameter names of an H2O Isolation forest model.
+     */
+    public static final Set<String> PARAMETER_NAMES =
+            ParametersBuilderUtil.getAllParametersNamesFor(water.bindings.pojos.IsolationForestParametersV3.class);
+
+    /**
      * The setter capable of assigning a value of a parameter to the right H2O REST POJO field.
      */
     private static final ParamsValueSetter<IsolationForestParametersV3> PARAMS_SETTER =
