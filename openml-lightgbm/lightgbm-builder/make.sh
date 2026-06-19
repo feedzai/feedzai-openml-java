b22e67119c47d23fa6878f225e41fed59047eacb
pedro.gandola <pedro.gandola@feedzai.com>
2026-06-19 20:28:52 +0100

    feat: Bump java version for 25 compatibility

--- openml-lightgbm/lightgbm-builder/make.sh
+++ openml-lightgbm/lightgbm-builder/make.sh
@@ -24,7 +24,8 @@ LIGHTGBMLIB_VERSION="$3"
 
 # git submodule
 echo "Checking out make-lightgbm project"
-git submodule update --init
+# Skipping submodule update to preserve local Dockerfile changes
+# git submodule update --init
 
 echo "Building LightGBM $LIGHTGBM_COMMIT_REF as lightgbmlib $LIGHTGBMLIB_VERSION"
 cd make-lightgbm
